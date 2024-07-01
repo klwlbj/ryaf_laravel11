6f68d09770c50837cc09cc35a27fa5ff925aee34
shikunsheng <1324281299@qq.com>
2024-07-01 17:41:12 +0800

    地址编码文件

---
+++ database/migrations/2024_07_01_073354_crate_areas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('parent_id')->comment('上线id');
+            $table->string('name')->comment('名称');
+            $table->tinyInteger('status')->default(1)->comment('状态');
+            $table->tinyInteger('level')->comment('级别');
+
+            $table->comment('地区表');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('areas');
+
+    }
+};
